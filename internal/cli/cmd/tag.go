9c91c8e444224b72a19f977dd792c826d242f3ed
j <21096932+julioc-lopez@users.noreply.github.com>
2023-08-29 18:45:58 +0000

    revert TagList field tag changes

--- internal/cli/cmd/tag.go
+++ internal/cli/cmd/tag.go
@@ -20,9 +20,9 @@ type Tag struct {
 type TagList struct {
 	Format     string   `group:"format" short:"f" placeholder:"TEMPLATE" help:"Pretty print the list using a custom template or one of the predefined formats: name, full, json, jsonl."`
 	Header     string   `group:"format"                                  help:"Arbitrary text printed at the start of the list."`
-	Footer     string   `group:"format" default:"\n"                     help:"Arbitrary text printed at the end of the list."`
-	Delimiter  string   `group:"format" short:"d" default:"\n"           help:"Print tags delimited by the given separator."`
-	Delimiter0 bool     `group:"format" short:"0" name:"delimiter0"      help:"Print tags delimited by ASCII NUL characters. This is useful when used in conjunction with 'xargs -0'."`
+	Footer     string   `group:format default:\n                         help:"Arbitrary text printed at the end of the list."`                                                           //nolint:govet
+	Delimiter  string   "group:format short:d default:\n                 help:\"Print tags delimited by the given separator.\""                                                           //nolint:govet
+	Delimiter0 bool     "group:format short:0 name:delimiter0            help:\"Print tags delimited by ASCII NUL characters. This is useful when used in conjunction with `xargs -0`.\"" //nolint:govet
 	NoPager    bool     `group:"format" short:"P"                        help:"Do not pipe output into a pager."`
 	Quiet      bool     `group:"format" short:"q"                        help:"Do not print the total number of tags found."`
 	Sort       []string `group:"sort"   short:"s" placeholder:"TERM"     help:"Order the tags by the given criterion."`
